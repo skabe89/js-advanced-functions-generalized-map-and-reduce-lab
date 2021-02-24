26c94c72d983183cad789c299ec55c08c68d98dc
Sean <skabe89@gmail.com>
2021-02-24 14:12:07 -0500

    commit tree fffbce354f06d06c7ab8d5b5e2b7c9f4a65ec910

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+
+let map = function(source, callback){
+  let place = []
+  for(let i = 0; i < source.length; i ++){
+    place.push(callback(source[i]))
+  }
+  return place
+}
+
+let reduce = function(source, callback, start = 0){
+  let total = start
+  for(let i = 0; i < source.length; i ++){
+    total = callback(source[i]) + total
+  }
+  return total
+}
